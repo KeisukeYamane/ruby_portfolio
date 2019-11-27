5d6799ccf9ba8b44ab2efe3e7c8bc9fffc437e4d
KeisukeYamane <keiroot.main.22@gmail.com>
2019-11-27 21:34:52 +0900

    create feature_user_test and add some tests

--- spec/features/users_spec.rb
+++ spec/features/users_spec.rb
@@ -1,5 +1,69 @@
 require 'rails_helper'
 
 RSpec.feature "Users", type: :feature do
-  pending "add some scenarios (or delete) #{__FILE__}"
+  #ユーザーを作成できるかどうか
+  scenario "user can create user's account" do
+
+    user = FactoryBot.attributes_for(:user)
+    visit root_path
+    click_link "新規登録"
+    expect(current_path).to eq new_user_path
+
+    expect {
+      fill_in "名前", with: user[:name]
+      fill_in "メールアドレス", with: user[:email]
+      fill_in "パスワード", with: user[:password]
+      fill_in "パスワード再入力", with: user[:password_confirmation]
+      click_button "作成" 
+    }.to change(User, :count).by(1)
+  end
+
+  scenario "user can edit user's account" do
+    user = FactoryBot.create(:user)
+    visit root_path
+    click_link "ログイン"
+    fill_in "メールアドレス", with: user.email
+    fill_in "パスワード", with: user.password
+    click_button "ログイン"
+    expect(page).to have_content "ログイン完了しました"
+    expect(current_path).to eq posts_page_index_path(page: 1)
+
+    click_link "Jhon"
+    expect(current_path).to eq user_path(id: user.id)
+    click_link "設定/削除"
+    click_on "ユーザー情報を編集する"
+    expect(current_path).to eq edit_user_path(id: user.id)
+
+    fill_in "user_name", with: "michel"
+    fill_in "user_password", with: "1111"
+    fill_in "user_password_confirmation", with: "1111"
+    click_button "編集する"
+    expect(current_path).to eq user_path(id: user.id)
+
+    expect(user.reload.name).to eq("michel")
+  end
+
+  scenario "user can delete user's account", js: true do
+    
+    user = FactoryBot.create(:user)
+    visit root_path
+    click_link "ログイン"
+    fill_in "メールアドレス", with: user.email
+    fill_in "パスワード", with: user.password
+    click_button "ログイン"
+    expect(page).to have_content "ログイン完了しました"
+    expect(current_path).to eq posts_page_index_path(page: 1)
+
+    expect {
+      click_link "Jhon"
+      expect(current_path).to eq user_path(id: user.id)
+      click_link "設定/削除"
+      click_on "アカウントを削除する"
+      # page.accept_confirm "本当にアカウントを削除しますか？投稿内容など完全に消去されます" do
+      #   click_link "OK"
+      # end
+      # page.driver.browser.switch_to.alert.accept
+      expect(current_path).to eq posts_page_index_path(page: 1)
+  }.to change(User, :count).by(-1)
+  end
 end
